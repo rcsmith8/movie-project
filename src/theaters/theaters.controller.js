efa1658fc02b4e7569909a7acc90f9e87c20e3ac
rcsmith8 <94479792+rcsmith8@users.noreply.github.com>
2022-12-06 12:28:19 -0600

    Update theaters.controller.js

--- src/theaters/theaters.controller.js
+++ src/theaters/theaters.controller.js
@@ -1,11 +1,24 @@
-const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
-const theatersService = require("./theaters.service");
+const service = require("./theaters.service");
 
-async function list(req, res, next) {
-  const data = await theatersService.list();
-  res.json({ data });
+async function list(req, res) {
+	const theaters = await service.list();
+
+	for(let theater of theaters) {
+		const movies = await service.listMovies(theater.theater_id);
+
+		theater["movies"] = movies;
+	}
+	console.log("line 11");
+	res.json({ data: theaters });
+}
+
+async function listSpecificMovie(req, res, next) {
+	if(res.locals.movie) {
+		return res.json({ data: await service.listTheaters(res.locals.movie.movie_id) });
+	}
+	next();
 }
 
 module.exports = {
-    list: asyncErrorBoundary(list)
+	list: [listSpecificMovie, list],
 }
