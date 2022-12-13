b2b871457a9b7e0e83e0b886b558c06f2e6c8ce1
rcsmith8 <94479792+rcsmith8@users.noreply.github.com>
2022-12-13 10:30:36 -0600

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -2,15 +2,7 @@ const path = require("path");
 
 require("dotenv").config();
 
-const {
-  NODE_ENV = "development",
-  DEVELOPMENT_DATABASE_URL,
-  PRODUCTION_DATABASE_URL,
-} = process.env;
-const URL =
-  NODE_ENV === "production"
-    ? PRODUCTION_DATABASE_URL
-    : DEVELOPMENT_DATABASE_URL;
+const { DATABASE_URL } = process.env;
 
 module.exports = {
   development: {
