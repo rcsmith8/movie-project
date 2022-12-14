000a173ac20f5789db4b488870ca3eecf64f18ad
rcsmith8 <94479792+rcsmith8@users.noreply.github.com>
2022-12-14 13:06:53 -0600

    Update 00_drop_tables.js

--- src/db/seeds/00_drop_tables.js
+++ src/db/seeds/00_drop_tables.js
@@ -1,9 +1,8 @@
 module.exports.seed = function (knex) {
-  // Deletes ALL existing entries
-  return knex("reviews")
-    .del()
-    .then(() => knex("movies_theaters").del())
-    .then(() => knex("critics").del())
-    .then(() => knex("movies").del())
-    .then(() => knex("theaters").del());
+  return knex
+    .raw("TRUNCATE TABLE reviews RESTART IDENTITY CASCADE")
+    .then(() => knex.raw("TRUNCATE TABLE movies_theaters RESTART IDENTITY CASCADE"))
+    .then(() => knex.raw("TRUNCATE TABLE critics RESTART IDENTITY CASCADE"))
+    .then(() => knex.raw("TRUNCATE TABLE movies RESTART IDENTITY CASCADE"))
+    .then(() => knex.raw("TRUNCATE TABLE theaters RESTART IDENTITY CASCADE"));
 };
